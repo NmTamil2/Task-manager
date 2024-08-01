43e5da45c8ec9c6165a60bd372ad0234647f0863
NmTamil2 <158686376+NmTamil2@users.noreply.github.com>
2024-08-01 10:28:59 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('taskManagerApp.urls')),
+    path('add/', views.add_task, name='add_task'),
+    path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('', views.view_all_tasks, name='view_all_tasks'),
+    path('filter/<str:priority>/', views.filter_tasks_by_priority, name='filter_tasks_by_priority'),
 ]
